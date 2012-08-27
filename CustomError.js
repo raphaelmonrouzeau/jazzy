91c9c625fdf2db44844ba7d4bb1d05e3a920fb15
Raphaël Monrouzeau <raphael.monrouzeau@gmail.com>
2012-08-27 15:25:31 +0200

    Added custom error constructor

---
+++ CustomError.js
@@ -0,0 +1,47 @@
+//
+// http://stackoverflow.com/questions/464359/custom-exceptions-in-javascript
+//
+// Only works in V8 / Node.JS.
+//
+var CustomError = exports = module.exports = (function() {
+
+    function define(obj, prop, value) {
+        Object.defineProperty(obj, prop, {
+            value: value,
+            configurable: true,
+            enumerable: false,
+            writable: true
+        });
+    }
+
+    return function(name, init, proto) {
+        var CustomError;
+        proto = proto || {};
+        function build(message) {
+            var self = this instanceof CustomError
+                ? this
+                : Object.create(CustomError.prototype);
+            Error.apply(self, arguments);
+            Error.captureStackTrace(self, CustomError);
+            if (message != undefined) {
+                define(self, 'message', String(message));
+            }
+            define(self, 'arguments', undefined);
+            define(self, 'type', undefined);
+            if (typeof init == 'function') {
+                init.apply(self, arguments);
+            }
+            return self;
+        }
+        eval('CustomError = function ' + name + '() {' +
+            'return build.apply(this, arguments); }');
+        CustomError.prototype = Object.create(Error.prototype);
+        define(CustomError.prototype, 'constructor', CustomError);
+        for (var key in proto) {
+            define(CustomError.prototype, key, proto[key]);
+        }
+        Object.defineProperty(CustomError.prototype, 'name', { value: name });
+        return CustomError;
+    }
+
+})();
